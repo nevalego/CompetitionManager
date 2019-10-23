b2fe0ec6b3c8161c85d4ce95b1fd984c3055796a
A.Llanos <42568525+aitorllanos99@users.noreply.github.com>
2019-10-23 19:05:59 +0200

    Add files via upload

---
+++ GestorCompeticiones/src/logic/dto/AtletaDto.java
@@ -0,0 +1,13 @@
+package logic.dto;
+
+public class AtletaDto {
+	public String nombre;
+	public String apellido;
+	public String email;
+	public String dni;
+	public String genero;
+	public String diaNacimiento;
+	public String mesNacimiento;
+	public String añoNacimiento;
+
+}
---
+++ GestorCompeticiones/src/logic/dto/InscripcionDto.java
@@ -0,0 +1,5 @@
+package logic.dto;
+
+public class InscripcionDto {
+
+}
